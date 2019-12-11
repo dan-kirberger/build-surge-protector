2cf699e6263787d4476f03bf28c970b4881039ab
Dan Kirberger <daniel.kirberger@target.com>
2019-12-10 18:42:39 -0600

    Setting up drone sdk

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# build-surge-protector
-Plugin for Drone that kills old builds when new ones are started
+# Drone build Surge Protector
+Plugin for Drone that kills old builds when new ones are started.
+
+### Why
+Do you have some expensive builds? (For example, large/slow test suites)
+
+Do you have some jerk that pushes a bunch of commits to their PR branch while working
+without caring about the results until they are done? (For example, Brian)
+
+Do you have an overworked drone cluster that occasionally can't keep up with the barrage described above?
+
+Try this maybe?
+
+### How
+```
+stuff
+```
+
+This plugin was only tested against Drone 0.8.x
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"github.com/drone/drone-go/drone"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2"
+	"os"
+)
+
+func getEnv(varName string) string {
+	value, _ := os.LookupEnv(varName)
+	return value
+}
+
+func checkEnv(varName string) {
+	_, exists := os.LookupEnv(varName)
+	if !exists {
+		fmt.Println("Must set environment variable: " + varName)
+		os.Exit(1)
+	}
+}
+
+func main() {
+	var token = getEnv("PLUGIN_TOKEN")
+	var host  = getEnv("PLUGIN_DRONE_HOST")
+	var orgName = getEnv("DRONE_REPO_OWNER")
+	var repoName = getEnv("DRONE_REPO_NAME")
+	//DRONE_REPO_OWNER
+	//DRONE_REPO_NAME
+	println("here i am")
+	config := new(oauth2.Config)
+	auther := config.Client(
+		context.Background(),
+		&oauth2.Token{
+			AccessToken: token,
+		},
+	)
+
+	// create the drone client with authenticator
+	client := drone.NewClient(host, auther)
+
+	// gets the current user
+	user, err := client.Self()
+	fmt.Println(user, err)
+
+	// gets the named repository information
+	repo, err := client.Repo(orgName, repoName)
+	fmt.Println(repo, err)
+}
